2dcb67a48ad00aa6a1d1964fadbb311aa0af6737
mark61-done <markoolando558@gmail.com>
2026-01-03 12:22:41 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,10 @@
+# variables
+# A variable is a container or a storage area that holds data.
+# A variable can store any data eg number, name, mathematical values
+
+# Assigning values to variables in python
+name = "John Doe"
+age = 30
+
+print("My name is",name,"I am",age,"years old")
+# Name and age are the variables and john doe and 30 are values so name and age are containers.
